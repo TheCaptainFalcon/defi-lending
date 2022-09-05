aa22a3b78f13b20c79d22f75a5ef28654436527e
Joseph <josephkim570@gmail.com>
2022-09-05 18:21:59 -0400

    Tulip functional code added. Note that the code is dependent on viewport being full screen. Default options would require additional code to press certain buttons. This approach is less error prone.

--- scrape.js
+++ scrape.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async function scrape() {
-    const browser =  await puppeteer.launch({ headless: true })
+    const browser =  await puppeteer.launch({ headless: false, defaultViewport: null })
     const page = (await browser.pages())[0]
     await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36");
-    // let link = 'https://solend.fi/dashboard';
+    let link = 'https://solend.fi/dashboard';
     await page.goto('https://solend.fi/dashboard', {waitUntil: 'domcontentloaded'})
     // await page.waitForTimeout(5000)
     // await page.waitForSelector('.Typography_secondary__MsHYD', {visible: true, waitUntil: 'domcontentloaded'})
@@ -72,6 +72,7 @@ const puppeteer = require('puppeteer');
 
     const solend_sol = {
         name : 'sol',
+        lending_protocol : 'solend',
         price : solend_sol_price,
         // ltv : sol_ltv,
         total_supply : solend_sol_supply,
@@ -84,6 +85,7 @@ const puppeteer = require('puppeteer');
 
     const solend_usdc = {
         name : 'usdc',
+        lending_protocol : 'solend',
         price : solend_usdc_price,
         total_supply : solend_usdc_supply,
         total_borrow : solend_usdc_borrow,
@@ -95,6 +97,7 @@ const puppeteer = require('puppeteer');
 
     const solend_usdt = {
         name : 'usdt',
+        lending_protocol : 'solend',
         price : solend_usdt_price,
         total_supply : solend_usdt_supply,
         total_borrow : solend_usdt_borrow,
@@ -110,30 +113,82 @@ const puppeteer = require('puppeteer');
 
     // navigate to next url (having multiple tabs performing tasks can put too much load on rpi)
 
-    // await page.goto('https://tulip.garden/lend', {waitUntil: 'domcontentloaded'});
-    // await page.waitForNetworkIdle();
-    // const 
+    // must be set otherwise responsiveness will hide data on tulip and require clicks
+    // already coded without on solend (oops)
+    await page.setViewport({ width: 1920, height: 1080})
 
-    // try {
-    //     await page.waitForSelector('.Typography_primary__ljjY8', {visible : true, waitUntil : 'domcontentloaded'})
-    //     await page.evaluate(() => document.querySelectorAll('.Typography_primary__ljjY8')[19].textContent)
-    //     // console does work hmm...
-    //     console.log(await page.evaluate(() => document.querySelectorAll('.Typography_primary__ljjY8')[19].textContent))
-    // } catch {
-    //     console.log("error", error)
-    // }
-   
-    
-        // let ltv = document.querySelectorAll('.Typography_primary__ljjY8')[19].textContent
-        // let total_supply = document.querySelectorAll('.Typography_primary__ljjY8')[20].textContent
-        // let total_borrow = document.querySelectorAll('.Typography_primary__ljjY8')[21].textContent
-        // let supply_apy = document.querySelectorAll('.Typography_primary__ljjY8')[22].textContent
-        // console.log(price)
-    
+    await page.goto('https://tulip.garden/lend', {waitUntil: 'domcontentloaded'});
+    await page.waitForNetworkIdle();
+    await page.waitForSelector('div.lend-table__row-item__cell-usd')
 
-// subsection solend for usdc
+    // in order to account for edgecase, replacing built in substring method to an alternative.
+    // using subtring 1 will eliminate the $ but can account for increasing digit counts
+    // adding m in zeros to keep data consistent
+    const millions = 1000000;
+    const tulip_usdc_supply_apy = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell')[2].textContent.replace('%' , '')));
+
+    // could use the same class in apy above, but there are too many extra variables that can upset the built in substring method.
+    const tulip_usdc_supply = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell-usd')[0].textContent.substring(1).replace('M' , ''))) * millions;
+
+    const tulip_usdc_borrow = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell-usd')[1].textContent.substring(1).replace('M', ''))) * millions;
+    const tulip_usdc_utilization = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell')[5].textContent.replace('%', '').trimEnd()));
+
+    // tulip usdt
+
+    const tulip_usdt_supply_apy = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell')[8].textContent.replace('%' , '')));
+    const tulip_usdt_supply = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell-usd')[2].textContent.substring(1).replace('M' , ''))) * millions;
+    const tulip_usdt_borrow = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell-usd')[3].textContent.substring(1).replace('M', ''))) * millions;
+    const tulip_usdt_utilization = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell')[11].textContent.replace('%', '').trimEnd()));
 
 
+    // tulip sol
+
+    const tulip_sol_supply_apy = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell')[20].textContent.replace('%' , '')));
+    const tulip_sol_supply = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell-usd')[6].textContent.substring(1).replace('M' , ''))) * millions;
+    const tulip_sol_borrow = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell-usd')[7].textContent.substring(1).replace('M', ''))) * millions;
+    const tulip_sol_utilization = await page.evaluate(() => parseFloat(document.querySelectorAll('div.lend-table__row-item__cell')[53].textContent.replace('%', '').trimEnd()));
+
+
+    const tulip_sol = {
+        name : 'sol',
+        lending_protocol : 'tulip',
+        total_supply : tulip_sol_supply,
+        total_borrow : tulip_sol_borrow,
+        supply_apy : tulip_sol_supply_apy,
+        utilization : tulip_sol_utilization,
+        date : date,
+        time : time,
+        day_of_week : dow
+    }
+
+    const tulip_usdc = {
+        name : 'usdc',
+        lending_protocol : 'tulip',
+        total_supply : tulip_usdc_supply,
+        total_borrow : tulip_usdc_borrow,
+        supply_apy : tulip_usdc_supply_apy,
+        utilization : tulip_usdc_utilization,
+        date : date,
+        time : time,
+        day_of_week : dow
+    }
+
+    const tulip_usdt = {
+        name : 'usdt',
+        lending_protocol : 'tulip',
+        total_supply : tulip_usdt_supply,
+        total_borrow : tulip_usdt_borrow,
+        supply_apy : tulip_usdt_supply_apy,
+        utilization : tulip_usdt_utilization,
+        date : date,
+        time : time,
+        day_of_week : dow
+    }
+
+
+    console.log(tulip_sol)
+    console.log(tulip_usdc)
+    console.log(tulip_usdt)
 
     await browser.close()
 
